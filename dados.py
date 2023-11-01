4497741e3f27c271df98ad1db26a7420fb8216e9
cleyson02 <cleysonsilva2002@gmail.com>
2023-11-01 19:29:13 -0300

    Funções
    Correção de bugs e inserção de mais funções com seus respectivos comentários

--- dados.py
+++ dados.py
@@ -1,14 +1,14 @@
-import sqlite3
+import sqlite3  # Importa o módulo sqlite3 para trabalhar com bancos de dados SQLite.
 
-# conectar ao banco de dados ou criar um novo banco de dados
-con = sqlite3.connect('dados.db')
+# Conectar ao banco de dados ou criar um novo banco de dados
+con = sqlite3.connect('dados.db')  # Cria uma conexão com o banco de dados 'dados.db'. Se o banco de dados não existir, ele será criado.
 
-# criando tabela de salas
+# Criando tabela de salas
 con.execute('CREATE TABLE salas(\
             id INTEGER PRIMARY KEY,\
-            sala TEXT)')
+            nome TEXT)')
 
-# criando tabela de usuários
+# Criando tabela de usuários
 con.execute('CREATE TABLE usuarios(\
             id INTEGER PRIMARY KEY,\
             nome TEXT,\
@@ -16,7 +16,7 @@ con.execute('CREATE TABLE usuarios(\
             telefone TEXT,\
             email TEXT)')
 
-#criando tabela de reservas
+# Criando tabela de reservas
 con.execute('CREATE TABLE reservas(\
             id INTEGER PRIMARY KEY,\
             id_sala INTEGER,\
--- view.py
+++ view.py
@@ -1,25 +1,64 @@
 import sqlite3
 
-# conectar ao banco de dados
+# Função para conectar ao banco de dados
 def connect():
     conn = sqlite3.connect('dados.db')
     return conn
 
-# função para inserir uma nova sala
+# Função para inserir uma nova sala no banco de dados
 def insert_room(nome):
-    conn = connect()
-    conn.execute("INSERT INTO salas(nome) VALUES(?)", (nome))
+    conn = connect()  # Conectar ao banco de dados
+    cursor = conn.cursor()  # Criar um cursor para executar consultas SQL
+    cursor.execute("INSERT INTO salas(nome) VALUES(?)", (nome,))  # Inserir uma nova sala
+    conn.commit()  # Confirmar a inserção
+    conn.close()  # Fechar a conexão com o banco de dados
 
-# função para inserir usuários
+# Função para remover uma sala com base no ID
+def remove_room(id):
+    conn = connect()  # Conectar ao banco de dados
+    cursor = conn.cursor()  # Criar um cursor para executar consultas SQL
+    cursor.execute("SELECT * FROM salas WHERE id = ?", (id,))  # Buscar a sala com o ID fornecido
+    existing_room = cursor.fetchone()  # Tentar buscar a sala com o ID fornecido
+
+    if existing_room is not None:
+        cursor.execute("DELETE FROM salas WHERE id = ?", (id,))  # Remover a sala com o ID fornecido
+        conn.commit()  # Confirmar a remoção
+        print(f"Sala com ID {id} foi removida com sucesso.")
+    else:
+        print(f"Sala com ID {id} não foi encontrada e não pode ser removida.")
+    
+    conn.close()  # Fechar a conexão com o banco de dados
+
+# Função para inserir informações de usuário no banco de dados
 def insert_user(nome, sobrenome, telefone, email):
-    conn = connect()
-    conn.execute("INSERT INTO usuarios(nome, sobrenome, telefone, email) VALUES (?, ?, ?, ?)", (nome, sobrenome, telefone, email))
+    conn = connect()  # Conectar ao banco de dados
+    cursor = conn.cursor()  # Criar um cursor para executar consultas SQL
+    cursor.execute("INSERT INTO usuarios(nome, sobrenome, telefone, email) VALUES (?, ?, ?, ?)", (nome, sobrenome, telefone, email))  # Inserir informações de usuário
+    conn.commit()  # Confirmar a inserção
+    conn.close()  # Fechar a conexão com o banco de dados
 
-# funcao para exibir as salas
+# Função para remover um usuário com base no ID
+def remove_user(id):
+    conn = connect()  # Conectar ao banco de dados
+    cursor = conn.cursor()  # Criar um cursor para executar consultas SQL
+    cursor.execute("SELECT * FROM usuarios WHERE id = ?", (id,))  # Buscar o usuário com o ID fornecido
+    existing_user = cursor.fetchone()  # Tentar buscar o usuário com o ID fornecido
+
+    if existing_user is not None:
+        cursor.execute("DELETE FROM usuarios WHERE id = ?", (id,))  # Remover o usuário com o ID fornecido
+        conn.commit()  # Confirmar a remoção
+        print(f"Usuário com ID {id} foi removido com sucesso.")
+    else:
+        print(f"Usuário com ID {id} não foi encontrado e não pode ser removido.")
+    
+    conn.close()  # Fechar a conexão com o banco de dados
+
+# Função para exibir informações das salas existentes no banco de dados
 def exibir_salas():
-    conn = connect()
-    salas = conn.execute("SELECT * FROM salas").fetchall()
-    conn.close()
+    conn = connect()  # Conectar ao banco de dados
+    cursor = conn.cursor()  # Criar um cursor para executar consultas SQL
+    salas = cursor.execute("SELECT * FROM salas").fetchall()  # Buscar informações das salas
+    conn.close()  # Fechar a conexão com o banco de dados
 
     if not salas:
         print("Nenhuma sala encontrada.")
@@ -31,4 +70,24 @@ def exibir_salas():
         print(f"Nome: {sala[1]}")
         print("\n")
 
-exibir_salas()
+# Função para exibir informações dos usuários existentes no banco de dados
+def exibir_usuarios():
+    conn = connect()  # Conectar ao banco de dados
+    cursor = conn.cursor()  # Criar um cursor para executar consultas SQL
+    usuarios = cursor.execute("SELECT * FROM usuarios").fetchall()  # Buscar informações dos usuários
+    conn.close()  # Fechar a conexão com o banco de dados
+
+    if not usuarios:
+        print("Nenhum usuário encontrado.")
+        return
+    
+    print("Usuários existentes: ")
+    for usuario in usuarios:
+        print(f"ID: {usuario[0]}")
+        print(f"Nome: {usuario[1]}")
+        print(f"Sobrenome: {usuario[2]}")
+        print(f"Telefone: {usuario[3]}")
+        print(f"Email: {usuario[4]}")
+        print("\n")
+
+remove_room(1)
